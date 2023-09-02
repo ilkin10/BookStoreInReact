cc389fed012b00bde366910f41fed91a5f80736b
ilkin <ilkin@MY_ASUS>
2023-09-03 01:38:47 +0400

    Done Filter Except GUID

--- src/Utils/NavbarUtil.js
+++ src/Utils/NavbarUtil.js
@@ -1,16 +1,20 @@
 import { Books, CurrentSetter } from "./CurrentList";
 
-
-
-export const filterBooks = (books,keyword) => {
-    let searchedBooks = [];
-    books.forEach(b => {
-        if(JSON.stringify(b).includes(keyword)){
-
-            searchedBooks.push(b);
-        }
-    });
-    console.log(searchedBooks)
-    return searchedBooks
-
-  };
+export const filterBooks = (books, keyword) => {
+  let searchedBooks = [];
+  books.forEach((b) => {
+    // Check if any of the properties contain the keyword
+    if (
+      b.Title.includes(keyword) ||
+      b.Author.includes(keyword) ||
+      b.Language.includes(keyword) ||
+      b.Publisher.includes(keyword) ||
+      b.Year.includes(keyword) ||
+      b.Pages.includes(keyword)
+    ) {
+      searchedBooks.push(b);
+    }
+  });
+  console.log(searchedBooks);
+  return searchedBooks;
+};
