3a7cb327264d84fc3651f4aea827b4378774f6dc
ilkin <ilkin@MY_ASUS>
2023-09-03 01:32:53 +0400

    Done SuccessFully

--- src/Components/BookDetails.js
+++ src/Components/BookDetails.js
@@ -1,7 +1,10 @@
 // BookDetails.js
 import React from "react";
-import './../Book.css';
+import "./../Book.css";
+import "./../Comments.css";
+
 import { useParams } from "react-router-dom";
+import Cookies from "js-cookie";
 
 function BookDetails({ books }) {
   // Access the book GUID from the route parameter
@@ -10,42 +13,141 @@ function BookDetails({ books }) {
   // Find the book with the matching GUID
   const book = books.find((book) => book.Id === Id);
 
+  Cookies.set("Name", "Matthew Twomey");
+  Cookies.set("Name2", "John Doe");
+
+  Cookies.set("Day", "2 days ago");
+  Cookies.set("Day2", "13 hours ago");
+
+  Cookies.set("Text", "This is some content from a media component.");
+  Cookies.set("Text2", "Amazing Book I have ever read");
+
+  const Name = Cookies.get("Name");
+  const Day = Cookies.get("Day");
+  const Text = Cookies.get("Text");
+  const Name2 = Cookies.get("Name2");
+  const Day2 = Cookies.get("Day2");
+  const Text2 = Cookies.get("Text2");
   if (!book) {
     return <div>Book not found</div>;
   }
-//   display: grid;
-//   place-items: center; /* Center both horizontally and vertically */
-//   height: 100vh; 
+  //   display: grid;
+  //   place-items: center; /* Center both horizontally and vertically */
+  //   height: 100vh;
   return (
-      <main>
-        <div style={{padding:"25x",display:"grid",placeItems:"center",height:"80vh"}} className="book-card">
-          <div className="book-card__cover">
-            <div className="book-card__book">
-              <div className="book-card__book-front">
-                <img
-                  className="book-card__img"
-                  src={book.CoverUrl}
-                  alt="Book cover"
-                />
-              </div>
-              <div className="book-card__book-back"></div>
-              <div className="book-card__book-side"></div>
+    <main>
+      <div
+        style={{
+          display: "grid",
+          placeItems: "center",
+        }}
+        className="book-card"
+      >
+        <div className="book-card__cover">
+          <div className="book-card__book">
+            <div className="book-card__book-front">
+              <img
+                className="book-card__img"
+                src={book.CoverUrl}
+                alt="Book cover"
+              />
             </div>
-          </div>
-          <div>
-            <div style={{textAlign:"center",fontWeight:"bolder"}} className="book-card__title">
-              {book.Title}
-            </div>
-            <div style={{fontSize:"20px",textAlign:"center"}} className="book-card__author">Book's Author : {book.Author}</div>
-            <div style={{fontSize:"20px",textAlign:"center"}} className="book-card__author">Book's Language : {book.Language}</div>
-            <div style={{fontSize:"20px",textAlign:"center"}} className="book-card__author">Book's Publisher : {book.Publisher}</div>
-            <div style={{fontSize:"20px",textAlign:"center"}} className="book-card__author">Book's Year : {book.Year}</div>
-            <div style={{fontSize:"20px",textAlign:"center"}} className="book-card__author">Book's Page Count : {book.Pages}</div>
-
-
+            <div className="book-card__book-back"></div>
+            <div className="book-card__book-side"></div>
           </div>
         </div>
-      </main>
+        <div>
+          <div
+            style={{ textAlign: "center", fontWeight: "bolder" }}
+            className="book-card__title"
+          >
+            {book.Title}
+          </div>
+          <div
+            style={{ fontSize: "20px", textAlign: "center" }}
+            className="book-card__author"
+          >
+            Book's Author : {book.Author}
+          </div>
+          <div
+            style={{ fontSize: "20px", textAlign: "center" }}
+            className="book-card__author"
+          >
+            Book's Language : {book.Language}
+          </div>
+          <div
+            style={{ fontSize: "20px", textAlign: "center" }}
+            className="book-card__author"
+          >
+            Book's Publisher : {book.Publisher}
+          </div>
+          <div
+            style={{ fontSize: "20px", textAlign: "center" }}
+            className="book-card__author"
+          >
+            Book's Year : {book.Year}
+          </div>
+          <div
+            style={{ fontSize: "20px", textAlign: "center" }}
+            className="book-card__author"
+          >
+            Book's Page Count : {book.Pages}
+          </div>
+          <hr></hr>
+          <div class="d-flex align-items-center justify-content-center">
+            <div class="container">
+              <div class="row justify-content-center">
+                <div class="col-lg-8">
+                  <div class="d-flex px-3 py-4 border border-bottom-0 border-light rounded-top">
+                    <div class="flex-shrink-0">
+                      <div class="avatar avatar-sm rounded-circle">
+                        <img
+                          class="avatar-img"
+                          src="https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&crop=faces&fit=crop&h=200&w=200"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                    <div class="flex-grow-1 ms-2 ms-md-3">
+                      <div class="d-flex align-items-baseline">
+                        <h6 class="me-2">{Name}</h6>
+                        <span class="badge py-1 bg-light text-muted fw-normal">
+                          {Day}
+                        </span>
+                      </div>
+                      <div>{Text}</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-8">
+                  <div class="d-flex px-3 py-4 border border-bottom-0 border-light rounded-top">
+                    <div class="flex-shrink-0">
+                      <div class="avatar avatar-sm rounded-circle">
+                        <img
+                          class="avatar-img"
+                          src="https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&crop=faces&fit=crop&h=200&w=200"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                    <div class="flex-grow-1 ms-2 ms-md-3">
+                      <div class="d-flex align-items-baseline">
+                        <h6 class="me-2">{Name2}</h6>
+                        <span class="badge py-1 bg-light text-muted fw-normal">
+                          {Day2}
+                        </span>
+                      </div>
+                      <div>{Text2}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br></br>
+        </div>
+      </div>
+    </main>
 
     // <div
     //   className="card mb-3"
